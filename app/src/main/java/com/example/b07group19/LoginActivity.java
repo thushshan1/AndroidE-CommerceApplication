1bc5958da2a7d25271d2eff8cf8aa744e56e81ea
Chenhao Zhou <zhouchenhao0227@gmail.com>
2023-07-25 01:11:46 -0400

    added login

--- app/src/main/java/com/example/b07group19/LoginActivity.java
+++ app/src/main/java/com/example/b07group19/LoginActivity.java
@@ -20,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ownerlogin);
+        setContentView(R.layout.activity_shopperlogin);
 
         mAuth = FirebaseAuth.getInstance();
 
