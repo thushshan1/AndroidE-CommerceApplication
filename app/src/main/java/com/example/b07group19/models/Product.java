5b8c5b72965627c3715858c29ba1f4fedba2d737
Jennifer Hou <jennifer.hoummm@gmail.com>
2023-08-05 09:25:48 +0800

    move product into model

--- app/src/main/java/com/example/b07group19/models/Product.java
+++ app/src/main/java/com/example/b07group19/models/Product.java
@@ -1,4 +1,27 @@
 package com.example.b07group19.models;
 
 public class Product {
+    public String product;
+    public double price;
+    public String brand;
+    public int quantity;
+    public double length;
+    public double width;
+    public double height;
+    public String turl;
+
+    public Product() {
+
+    }
+    public Product(String product, double price, String brand, int quantity, double length, double width, double height, String turl) {
+        this.product = product;
+        this.price = price;
+        this.brand = brand;
+        this.quantity = quantity;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.turl = turl;
+    }
+
 }
